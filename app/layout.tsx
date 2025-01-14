3d6ceec938ad6ec4185ac495c7f53d841f7e410b
Jeff Claybrook <jeffclaybrook@Jeffs-MBP.home>
2025-01-14 11:02:37 -0600

    Updated inter export

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google"
 import "./global.css"
 
-export const inter = Inter({ subsets: ["latin" ]})
+const inter = Inter({ subsets: ["latin" ]})
 
 export default function RootLayout({
   children,
